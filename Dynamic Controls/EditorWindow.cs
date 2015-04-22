388eb84fbf68b587c1f42ef3eb8d52c2085eb7db
Crzyrndm <joshcrawfy@gmail.com>
2015-04-23 10:30:22 +1200

    Initial Commit

---
+++ Dynamic Controls/EditorWindow.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Dynamic_Controls
+{
+    [KSPAddon(KSPAddon.Startup.EveryScene, false)]
+    class EditorWindow : MonoBehaviour
+    {
+        public static ModuleDynamicDeflection moduleToDraw;
+        string dynPressure = "";
+        string deflection = "";
+
+        Rect windowRect = new Rect(300, 300, 0, 0);
+        public void Start()
+        {
+            if (!(HighLogic.LoadedSceneIsFlight || HighLogic.LoadedSceneIsEditor))
+                return;
+
+            moduleToDraw = null;
+        }
+
+        public void Update()
+        {
+            if (!(HighLogic.LoadedSceneIsFlight || HighLogic.LoadedSceneIsEditor))
+                return;
+
+            if (Input.GetMouseButtonDown(0))
+            {
+                Vector2 mouse = Input.mousePosition;
+                mouse.y = Screen.height - mouse.y;
+                if (!windowRect.Contains(mouse))
+                    moduleToDraw = null;
+            }
+        }
+
+        public void OnGUI()
+        {
+            if (!(HighLogic.LoadedSceneIsFlight || HighLogic.LoadedSceneIsEditor))
+                return;
+
+            if (moduleToDraw != null)
+                windowRect = GUILayout.Window(7463908, windowRect, drawWindow, "");
+        }
+
+        private void drawWindow(int id)
+        {
+            foreach (List<float> list in moduleToDraw.deflectionAtPressure)
+            {
+                GUILayout.BeginHorizontal();
+                if (GUILayout.Button("Remove Key", GUILayout.Width(100)))
+                {
+                    list[1] = -1;
+                    windowRect.height = 0;
+                }
+                GUILayout.Label("\tQ (kPa)", GUILayout.Width(80));
+                list[0] = float.Parse(GUILayout.TextField(list[0].ToString("0.##"), GUILayout.Width(100)));
+                GUILayout.Label("\t% Deflect", GUILayout.Width(100));
+                list[1] = float.Parse(GUILayout.TextField(list[1].ToString("0.#"), GUILayout.Width(100)));
+                GUILayout.EndHorizontal();
+            }
+            GUILayout.Space(40);
+            GUILayout.BeginHorizontal();
+            if (GUILayout.Button("Add Key", GUILayout.Width(100)))
+            {
+                List<float> newEntry = new List<float>();
+                newEntry.Add(Mathf.Abs(float.Parse(dynPressure)));
+                newEntry.Add(Mathf.Abs(float.Parse(deflection)));
+                moduleToDraw.deflectionAtPressure.Add(newEntry);
+                dynPressure = deflection = "";
+            }
+            GUILayout.Label("\tQ (kPa)", GUILayout.Width(80));
+            dynPressure = GUILayout.TextField(dynPressure, GUILayout.Width(100));
+            GUILayout.Label("\t % deflect", GUILayout.Width(100));
+            deflection = GUILayout.TextField(deflection, GUILayout.Width(100));
+            GUILayout.EndHorizontal();
+
+            moduleToDraw.deflectionAtPressure = moduleToDraw.deflectionAtPressure.OrderBy(x => x[0]).ToList();
+            moduleToDraw.deflectionAtPressure.RemoveAll(l => l[1] < 0); // set deflection less than zero to remove from list
+
+            if (GUILayout.Button("Copy to all"))
+            {
+                foreach (Part p in moduleToDraw.vessel.Parts)
+                {
+                    if (p == null)
+                        continue;
+
+                    if (p.Modules.Contains("ModuleDynamicDeflection"))
+                        (p.Modules["ModuleDynamicDeflection"] as ModuleDynamicDeflection).deflectionAtPressure = moduleToDraw.deflectionAtPressure;
+                }
+            }
+
+            GUI.DragWindow();
+        }
+    }
+}
---
+++ Dynamic Controls/ModuleDynamicDeflection.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using System.Reflection;
+
+namespace Dynamic_Controls
+{
+    public class ModuleDynamicDeflection : PartModule
+    {
+        public List<List<float>> deflectionAtPressure; // int[0] = q, int[1] = deflection
+        private bool usingFAR;
+
+        private PartModule module;
+        private FieldInfo farValToSet;
+
+        [KSPField(guiActive = true, guiName = "Max Deflect", guiActiveEditor = false)]
+        public float deflection;
+
+        [KSPField(guiActive = true, guiName = "Deflection", guiActiveEditor = false)]
+        public float currentDeflection;
+
+        public override void OnLoad(ConfigNode node) // dynamic deflection node with actions and events subnodes
+        {
+            if (deflectionAtPressure == null)
+                deflectionAtPressure = new List<List<float>>();
+
+            foreach (string s in node.GetValues("key"))
+            {
+                string[] kvp = s.Split(',');
+                List<float> val = new List<float>() { Mathf.Abs(float.Parse(kvp[0].Trim())), Mathf.Abs(float.Parse(kvp[1].Trim())) };
+                deflectionAtPressure.Add(val);
+            }
+        }
+
+
+        public void Start()
+        {
+            usingFAR = AssemblyLoader.loadedAssemblies.Any(a => a.assembly.GetName().Name.Equals("FerramAerospaceResearch", StringComparison.InvariantCultureIgnoreCase)
+                                                                || a.assembly.GetName().Name.Equals("NEAR", StringComparison.InvariantCultureIgnoreCase));
+            if (part.Modules.Contains("FARControllableSurface"))
+                module = part.Modules["FARControllableSurface"];
+            else if (part.Modules.Contains("ModuleControlSurface"))
+                module = part.Modules["ModuleControlSurface"];
+
+            if (usingFAR)
+            {
+                farValToSet = module.GetType().GetField("maxdeflect");
+                deflection = (float)farValToSet.GetValue(module);
+            }
+            else
+                deflection = (module as ModuleControlSurface).ctrlSurfaceRange;
+            if (deflectionAtPressure.Count == 0)
+                deflectionAtPressure.Add(new List<float>() { 0, deflection });
+        }
+
+        public void Update()
+        {
+            foreach (Part p in part.symmetryCounterparts)
+            {
+                if (p != null)
+                {
+                    (p.Modules["ModuleDynamicDeflection"] as ModuleDynamicDeflection).deflectionAtPressure = deflectionAtPressure;
+                }
+            }
+        }
+
+        public void FixedUpdate()
+        {
+            if (!HighLogic.LoadedSceneIsFlight)
+                return;
+
+            if (deflectionAtPressure.Count == 0)
+                deflectionAtPressure.Add(new List<float>() { 0, 100 });
+
+            if (deflectionAtPressure.Count == 1)
+                return;
+
+            float dynPres = getQ() / 1000; // kPa
+            List<float> minLerp = null, maxLerp = null;
+            foreach (List<float> x in deflectionAtPressure)
+            {
+                if (x[0] < dynPres)
+                    minLerp = x;
+                else
+                {
+                    maxLerp = x;
+                    break;
+                }
+            }
+            if (minLerp == null)
+                currentDeflection = maxLerp[1] * deflection / 100; // dynamic pressure too high
+            else if (maxLerp == null)
+                currentDeflection = minLerp[1] * deflection / 100; // dynamic pressure too low
+            else
+                currentDeflection = (minLerp[1] + (dynPres - minLerp[0]) / (maxLerp[0] - minLerp[0]) * (maxLerp[1] - minLerp[1])) * deflection / 100;
+
+            if (usingFAR)
+                farValToSet.SetValue(module, currentDeflection);
+            else
+                (module as ModuleControlSurface).ctrlSurfaceRange = currentDeflection;
+        }
+
+        private void OnMouseOver()
+        {
+            if (Input.GetKeyDown(KeyCode.K))
+                EditorWindow.moduleToDraw = this;
+        }
+
+        public override void OnSave(ConfigNode node)
+        {
+            base.OnSave(node);
+            if (deflectionAtPressure == null)
+                return;
+
+            foreach (List<float> pair in deflectionAtPressure)
+                node.AddValue("key", pair[0].ToString() + "," + pair[1].ToString());
+        }
+
+        public void OnDestroy()
+        {
+
+        }
+
+        private void LateUpdate()
+        {
+            dumpLog();
+        }
+
+        // list of everything to log in the next update from all loaded modules
+        static List<object> toLog = new List<object>();
+        private void Log(object objectToLog)
+        {
+            toLog.Add(objectToLog);
+        }
+
+        private void dumpLog()
+        {
+            if (toLog.Count == 0)
+                return;
+
+            string s = "";
+            foreach (object o in toLog)
+                s += o.ToString() + "\r\n";
+            Debug.Log(s);
+
+            toLog.Clear();
+        }
+
+        private float getQ() // roughly. no compensation for mach or temperature
+        {
+            return (float)(FlightGlobals.getAtmDensity(FlightGlobals.getStaticPressure(vessel.altitude, vessel.mainBody)) * vessel.srf_velocity.magnitude * vessel.srf_velocity.magnitude * 0.5);
+        }
+    }
+}
